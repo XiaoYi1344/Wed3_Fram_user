edf4b8408205b80429a6c72dec653974dc169d21
XiaoYi1344 <tieutyty1404@gmail.com>
2025-07-02 08:35:50 +0700

    Fix để deloy lần 1

--- src/app/logout/page.tsx
+++ src/app/logout/page.tsx
@@ -1,11 +1,8 @@
-import LogoutButton from "@/components/Logout/Logout";
+import Logout from "@/components/Logout/Logout";
 
 const LogoutPage = () => {
   return (
-    <div className="max-w-sm mx-auto mt-20 p-4 shadow-lg bg-white rounded-lg">
-      <h1 className="text-xl font-bold mb-4">Bạn có chắc chắn muốn đăng xuất?</h1>
-      <LogoutButton />
-    </div>
+    <Logout />
   );
 };
 
